fd51bbce845f21dd4de7388fc74a410a03956476
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-11 23:00:44 +0330

    feat(auth): send otp for authentication in auth controller

---
+++ src/modules/auth/auth.messages.js
@@ -0,0 +1,9 @@
+const AuthMessage = {
+    SendOTPSuccessfully: 'send otp successfully',
+    OtpCodeNotExpired: 'otp code not expired please try later',
+    NotFound: 'user not found',
+}
+
+module.exports = {
+    AuthMessage,
+}
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,6 +1,49 @@
+const createHttpError = require('http-errors')
+const UserModel = require('../user/user.model')
+const autoBind = require('auto-bind')
+const { AuthMessage } = require('./auth.messages')
+const { randomInt } = require('crypto')
+
 class AuthService {
-    async sendOTP(mobile) {}
+    #model
+
+    constructor() {
+        this.#model = UserModel
+        autoBind(this)
+    }
+    async sendOTP(mobile) {
+        const user = this.#model.findOne({ mobile })
+        const now = new Date().getTime()
+        const otp = {
+            code: randomInt(10000, 99999),
+            expiredIn: now + 1000 * 60 * 2,
+        }
+
+        if (!user) {
+            const newUser = await this.#model.create({
+                mobile,
+                otp,
+            })
+            return newUser
+        }
+
+        if (user.otp && user.otp.expiresIn > now) {
+            throw new createHttpError.BadRequest(AuthMessage.OtpCodeNotExpired)
+        }
+
+        user.otp = otp
+        await user.save()
+
+        return user
+    }
+
     async checkOTP(mobile, code) {}
+
+    async checkExistByMobile(mobile) {
+        const user = this.#model.findOne({ mobile })
+        if (!user) throw new createHttpError.NotFound(AuthMessage.NotFound)
+        return user
+    }
 }
 
 module.exports = new AuthService()
