5a37f850744befe4221e7a468f07686b588a728c
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-11 21:36:01 +0330

    feat(swagger): config swagger

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const swaggerConfig = require('./src/config/swagger.config')
 dotenv.config()
 
 const main = async () => {
     const app = express()
     const port = process.env.PORT
     require('./src/config/mongoose.config')
+    swaggerConfig(app)
     app.listen(port, () => {
         console.log(`server started: http://localhost:${port}\n`)
     })
---
+++ src/config/swagger.config.js
@@ -0,0 +1,24 @@
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerConfig = (app) => {
+    const swaggerDocument = swaggerJsDoc({
+        swaggerDefinition: {
+            info: {
+                title: 'Divar Node-JS',
+                description: '',
+
+
+
+                
+                version: '1.0.0',
+            },
+        },
+        apis: [],
+    })
+
+    const swagger = swaggerUi.setup(swaggerDocument, {})
+    app.use('/swagger', swaggerUi.serve, swagger)
+}
+
+module.exports = swaggerConfig
