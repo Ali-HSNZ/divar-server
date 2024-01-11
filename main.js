adbb0fed31bd98bc8f713679b3ec711958a67fe4
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-11 23:46:23 +0330

    feat: error handler

--- main.js
+++ main.js
@@ -2,16 +2,29 @@ const express = require('express')
 const dotenv = require('dotenv')
 const swaggerConfig = require('./src/config/swagger.config')
 const mainRouter = require('./src/app.routes')
+const NotFoundHandler = require('./src/common/exception/not-found.handler')
+const AllExceptionHandler = require('./src/common/exception/all-exception.handler')
 dotenv.config()
 
 const main = async () => {
     const app = express()
     const port = process.env.PORT
+
+    // config mongoDB
     require('./src/config/mongoose.config')
-    swaggerConfig(app)
+
+    // config app and Routes
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use(mainRouter)
+
+    // config swagger
+    swaggerConfig(app)
+
+    // Error Handlers
+    NotFoundHandler(app)
+    AllExceptionHandler(app)
+
     app.listen(port, () => {
         console.log(`server started: http://localhost:${port}\n`)
     })
