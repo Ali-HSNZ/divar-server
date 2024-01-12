59478b7d0317eab9016cf50968f78bf6607f193f
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-12 14:17:27 +0330

    feat(auth): set 'access_token' from  brawser after check-otp

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const swaggerConfig = require('./src/config/swagger.config')
 const mainRouter = require('./src/app.routes')
 const NotFoundHandler = require('./src/common/exception/not-found.handler')
 const AllExceptionHandler = require('./src/common/exception/all-exception.handler')
+const cookieParser = require('cookie-parser')
 dotenv.config()
 
 const main = async () => {
@@ -16,6 +17,7 @@ const main = async () => {
     // config app and Routes
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
+    app.use(cookieParser(process.env.COOKIE_SECRET_KEY))
     app.use(mainRouter)
 
     // config swagger
