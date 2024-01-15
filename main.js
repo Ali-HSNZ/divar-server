1bb312303dd8a2b6c4f8fb59361fbb90627ea5dd
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-15 21:51:17 +0330

    feat(app): use morgan middleware

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const mainRouter = require('./src/app.routes')
 const NotFoundHandler = require('./src/common/exception/not-found.handler')
 const AllExceptionHandler = require('./src/common/exception/all-exception.handler')
 const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
 dotenv.config()
 
 const main = async () => {
@@ -18,6 +19,7 @@ const main = async () => {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use(cookieParser(process.env.COOKIE_SECRET_KEY))
+    app.use(morgan('dev'))
     app.use(mainRouter)
 
     // config swagger
